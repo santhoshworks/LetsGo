65b480658c136a281b345fd0db0239433342707b
Santhosh Palanisamy <santhoshpalanisamy@Santhoshs-MacBook-Pro.local>
2018-03-11 12:00:49 -0400

    modal communication, card restructure

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
+import { VoteComponent } from '../vote/vote.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,11 +11,18 @@ import { Observable } from 'rxjs/Observable';
 export class DashboardComponent implements OnInit {
   db: any;
   items: Observable<any[]>;
+  selectedPoll: any;
+  @ViewChild(VoteComponent)
+  private voteComponent: VoteComponent;
+
   constructor(db: AngularFireDatabase) {
     this.items = db.list('letsgoplaces/Polls').valueChanges();
   }
 
   ngOnInit() {
   }
-
+  setSelectedPoll(poll) {
+    this.selectedPoll = poll;
+    this.voteComponent.showModal();
+  }
 }
--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { ModalDirective } from 'angular-bootstrap-md/modals/modal.directive';
 @Component({
   selector: 'app-vote',
   templateUrl: './vote.component.html',
   styleUrls: ['./vote.component.scss']
 })
 export class VoteComponent implements OnInit {
+  @Input() selectedPoll: any;
+  @ViewChild(ModalDirective)
+  private modal: ModalDirective;
   constructor(
   ) {
   }
 
   ngOnInit() {
         // initialize scrollspy
+  }
+  showModal() {
+    this.modal.show();
   }
 }
